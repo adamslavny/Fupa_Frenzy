bc0b5637a7884b7a19d3d5d5216616045744840f
Adam Slavny <55219984+adamslavny@users.noreply.github.com>
2021-04-19 11:35:58 -0400

    Upload assets

---
+++ assets/DisableCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCollider : MonoBehaviour
+{
+   private Collider playerCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Disable() //Disables the hitbox on player so after game ends, burgers can hit the player w/o affecting anything
+    {
+        GameObject playa = GameObject.FindWithTag("Player");
+        playerCollider = playa.GetComponent<Collider>();
+        playerCollider.enabled = !playerCollider.enabled;
+
+    }
+}
